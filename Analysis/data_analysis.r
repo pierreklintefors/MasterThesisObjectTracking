002fa1174ad74fd78aab91de5e3ae7ae168ec5f4
Pierre <bev15pkl@student.lu.se>
2021-04-01 15:24:30 +0200

    Updated r script

--- Analysis/data_analysis.r
+++ Analysis/data_analysis.r
@@ -3,56 +3,78 @@ rm(list=ls(all=TRUE))
 graphics.off()
 
 # Check that required packages are installed:
-#want = c("")
-#have = want %in% rownames(installed.packages())
-#if ( any(!have) ) { install.packages( want[!have] ) }
+want = c("dplyr")
+have = want %in% rownames(installed.packages())
+if ( any(!have) ) { install.packages( want[!have] ) }
 
+library(dplyr)
 #Set working directory that contains output files
 setwd("~/MasterThesisObjectTracking/Analysis")
 
 trackers_list = c('CSRT', 'GOTURN', 'KCF', 'MEDIANFLOW', 'MIL', 'MOSSE','TLD', 'yolov4-deepsort')
 
+
 for (tracker in 1:length(trackers_list)){
   assign(paste(trackers_list[tracker],'_data', sep = ''), 
-         read.csv2(file = paste('TrackersOutput/', trackers_list[tracker], '_output/output.csv', sep = ''), sep = ','))
+         read.csv2(file = paste('TrackersOutput/', trackers_list[tracker], '_output/output.csv', sep = ''), sep = ',', stringsAsFactors = FALSE))
   assign(paste('gt_', trackers_list[tracker], sep = ''), 
-          read.csv2(file = paste('TrackersOutput/', trackers_list[tracker], '_output/gt_bbox.csv', sep = ''), sep = ','))
+          read.csv2(file = paste('TrackersOutput/', trackers_list[tracker], '_output/gt_bbox.csv', sep = ''), sep = ',', stringsAsFactors = FALSE))
 }
 
+correct_gt_dataset <- function(gt){
+  gt$frame = gt$frame +1
+  doubles = c()
+  
+  #check after doubles
+  for (j in 2:nrow(gt)){
+    if(gt$frame[j]==gt$frame[j-1] && gt$outside[j-1]==1){doubles = gt[j, 'outside']=1}
+  }
+
+  
+  gt = distinct(gt)
+  
+  return(gt)
+}
 
 calculate_performance <- function(dataframe, gt){
   
-  in_roi = c()
-  xdiff = c()
-  ydiff = c()
+  xdiff = 0
+  ydiff = 0
+  roi = 30
     
-  for (i in 2:nrow(gt)){
-    if(gt$frame[i]==gt$frame[i-1] && gt$outside[i-1]==1){gt = gt[-i,]}
-  }
-  
+
   dataframe$bbox_center_x = dataframe$img_center_x + dataframe$Distance_x
   dataframe$bbox_center_y = dataframe$img_center_y + dataframe$Distance_y
   dataframe$gt_object_center_xdiff = NA
   dataframe$gt_object_center_xdiff = NA
+  dataframe$gt_in_roi = NA
   
 
   
-  for (j in 2:nrow(gt)){
+  for (j in 1:nrow(gt)){
     xdiff = as.numeric(dataframe[gt[j,'frame'], 'bbox_center_x']) - as.numeric(gt[j,'obj_center_x'])
     ydiff = as.numeric(dataframe[gt[j,'frame'], 'bbox_center_y']) - as.numeric(gt[j,'obj_center_y'])
     dataframe[gt[j,'frame'], 'gt_object_center_xdiff'] = xdiff
+    dataframe[gt[j,'frame'], 'gt_object_center_ydiff'] = ydiff
+    
+    if (abs(xdiff)+abs(dataframe$Distance_x) < roi && abs(ydiff) + abs(dataframe$Distance_y) < roi ){dataframe[gt[j,'frame'], 'gt_in_roi'] = TRUE}
+    else {dataframe[gt[j,'frame'], 'gt_in_roi'] = FALSE}
   }
   
   
   
-  #dataframe$gt_object_center_xdiff = xdiff
-  #dataframe$gt_object_center_ydiff = ydiff
+
 
   return(dataframe)
 }
 
-CSRT_data = calculate_performance(CSRT_data, gt_CSRT)
+df_name_list = c("CSRT_data", "GOTURN_data", "KCF_data", "MEDIANFLOW_data", "MIL_data", "MOSSE_data", "TLD_data", "`yolov4-deepsort_data`")
+gt_df_list = c("gt_CSRT", "gt_GOTURN", "gt_KCF", "gt_MEDIANFLOW", "gt_MIL", "gt_MOSSE", "gt_TLD", "gt_yolov4-deepsort")
 
 
+for(df in 1:length(gt_df_list)) {assign(paste(gt_df_list[df], '_corr', sep = ''), correct_gt_dataset(get(gt_df_list[df])))}
+
+for(df in 1:length(df_name_list)) {assign(get(df_name_list[df]), calculate_performance(get(df_name_list[df], get(gt_df_list[df]))))}
   
 
+names(df_name_list[1])
